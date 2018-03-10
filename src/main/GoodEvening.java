4fc72e269655d4f18d3d2f2359b6faca02f49958
nodir07@gmail.com <nodir07@gmail.com>
2018-03-10 12:05:19 -0500

    massage

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -1,5 +1,7 @@
 package main;
 
 public class GoodEvening {
-
+public static void main(String[] args) {
+	System.out.println("Good Evening Class!");
+}
 }
--- src/main/GoodMoring.java
+++ src/main/GoodMoring.java
@@ -1,5 +1,7 @@
 package main;
 
 public class GoodMoring {
-
+public static void main(String[] args) {
+	System.out.println("Good Morning Class!");
+}
 }
